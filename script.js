6d383d3acacd24fa011b0f5c764ba8c29e5f1daa
Marty Fay <marty.r.fay@gmail.com>
2023-05-30 13:50:41 -0300

    Collect data from form.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let myLibrary = [];
 const bookDisplay = document.querySelector(".books");
 const bookFormButton = document.querySelector("#display-form-button");
 const bookForm = document.querySelector(".book-form");
-const formString = `<form action="#" method="post">
+const formString = `<form>
     <div class="form-group">
     <label for="title">Title:</label
     ><input type="text" name="title" id="title" required />
@@ -34,7 +34,7 @@ const formString = `<form action="#" method="post">
         </div>
     </fieldset>
     </div>
-    <button type="submit">Submit</button>
+    <button id="submit-button" type="button">Submit</button>
 </form>`;
 const template = document.createElement("template");
 template.innerHTML = formString;
@@ -47,13 +47,16 @@ function Book(title, author, pages, hasRead) {
   this.author = author;
   this.pages = pages;
   this.hasRead = hasRead ? "have read" : "have not read";
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead}.`;
-  };
 }
 
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead}.`;
+};
+
 function addBookToLibrary(book) {
+  console.log(book.info());
   if (book instanceof Book) myLibrary.push(book);
+  console.log(myLibrary);
   return;
 }
 
@@ -65,4 +68,28 @@ function displayBooks() {
 
 function displayForm() {
   bookForm.appendChild(formHtml);
+  const submitButton = document.querySelector("#submit-button");
+  submitButton.addEventListener("click", submitBook);
+  // submitButton.addEventListener("click", () => (bookForm.textContent = ""));
+}
+
+function submitBook() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const hasRead = document.getElementById("yes").checked;
+  // console.log(
+  //   "author: " +
+  //     author +
+  //     " title: " +
+  //     title +
+  //     " pages: " +
+  //     pages +
+  //     " has read: " +
+  //     hasRead
+  // );
+  if (title === "") return alert("title is a required field");
+  if (author === "") return alert("author is a required field");
+  if (pages === "") return alert("pages is a required field");
+  addBookToLibrary(new Book(title, author, pages, hasRead));
 }
